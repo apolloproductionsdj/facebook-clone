17d80858355502fcc02079b7f873f0dbb4411335
apolloproductionsdj <apolloproductiosdj@gmail.com>
2020-08-29 11:06:12 -0700

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYxPGEFDabnE2nZ_AwMee4iUPpC7MhPWU",
+  authDomain: "facebook-clone-45d7c.firebaseapp.com",
+  databaseURL: "https://facebook-clone-45d7c.firebaseio.com",
+  projectId: "facebook-clone-45d7c",
+  storageBucket: "facebook-clone-45d7c.appspot.com",
+  messagingSenderId: "820040300705",
+  appId: "1:820040300705:web:cc93118994af01736198e0",
+  measurementId: "G-PRZG61ZWCJ"
+};
