e471f9b03738c94a92826ef8f7724f2eb824914e
Mike Torres <apolloproductionsdj@gmail.com>
2020-09-05 13:52:38 -0700

    prepared the datalayer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from './Header.js';
 import Sidebar from './Sidebar.js';
 import Feed from './Feed.js';
 import Widgets from './Widgets.js';
+import Login from './Login.js';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <>
       <Header />
+
         <div className="app__body">
           <Sidebar />
           <Feed />
           <Widgets />
         </div>
-
+        </>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCYxPGEFDabnE2nZ_AwMee4iUPpC7MhPWU",
   authDomain: "facebook-clone-45d7c.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:820040300705:web:cc93118994af01736198e0",
   measurementId: "G-PRZG61ZWCJ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
